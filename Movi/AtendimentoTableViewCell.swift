0a5e32752983c92a5a6fda8830d604cf11f93c3d
Maurício Lorenzetti <mauricio.lorenzetti.bezerra@gmail.com>
2018-04-08 15:57:24 -0300

    reclamacoes screen

---
+++ Movi/AtendimentoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AtendimentoTableViewCell.swift
+//  Movi
+//
+//  Created by Mauricio Lorenzetti on 08/04/18.
+//  Copyright © 2018 Filipe Marques. All rights reserved.
+//
+
+import UIKit
+
+class AtendimentoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+}
