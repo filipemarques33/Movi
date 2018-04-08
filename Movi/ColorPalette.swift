d4d9405be695430cbe22e4c00106d8fed37790b1
Unknown <fi.marques33@gmail.com>
2018-04-08 11:22:38 -0300

    Project Base

---
+++ Movi/ColorPalette.swift
@@ -0,0 +1,33 @@
+//
+//  ColorPalette.swift
+//  Movi
+//
+//  Created by Filipe Marques on 08/04/18.
+//  Copyright © 2018 Filipe Marques. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var lightNavyBlue: UIColor {
+        return UIColor(red: 46.0 / 255.0, green: 45.0 / 255.0, blue: 133.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var dodgerBlue: UIColor {
+        return UIColor(red: 70.0 / 255.0, green: 138.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var paleGrey: UIColor {
+        return UIColor(red: 244.0 / 255.0, green: 244.0 / 255.0, blue: 246.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var white: UIColor {
+        return UIColor(white: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var cloudyBlue: UIColor {
+        return UIColor(red: 188.0 / 255.0, green: 187.0 / 255.0, blue: 216.0 / 255.0, alpha: 1.0)
+    }
+    
+}
